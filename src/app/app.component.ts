43f7ca626391f98fa1178ec3e7b6f279f71fdbed
pfarmitage <47189004+pfarmitage@users.noreply.github.com>
2023-03-21 22:56:04 +0000

    3 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from './task/task';
+import { TaskDialogResult, TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-root',
@@ -17,5 +21,43 @@ export class AppComponent {
       description: 'Using Firebase and Angular create a Kanban app!'
     }
   ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
 
+  
+  constructor(private dialog: MatDialog) {}
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult|undefined) => {
+        if (!result) {
+          return;
+        }
+        this.todo.push(result.task);
+      });
+  }
+  
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
